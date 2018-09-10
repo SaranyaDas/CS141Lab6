7cf9bec17dc6d84b1c4867e5f2f76258a39c8343
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-09-10 10:30:03 -0400

    Update 2b.cpp

--- 2b.cpp
+++ 2b.cpp
@@ -1,14 +1,19 @@
 //Library
 #include <iostream>
 using namespace std;
+//Void function
 void flag (int x, int y, int &z) {
-z = x + y;
+  //Adds two integers
+  z = x + y;
 }
 int main () {
-int a, b, c;
-cout << "Enter two numbers: " << endl;
-cin >> a >> b;
-flag (a, b, c);
-cout << "The sum is " << c << "." << endl;
-return 0;
+  //Ask user for two numbers
+  int a, b, c;
+  cout << "Enter two numbers: " << endl;
+  cin >> a >> b;
+  //Call function with numbers as arguments
+  flag (a, b, c);
+  //Display sum
+  cout << "The sum is " << c << "." << endl;
+  return 0;
 }
