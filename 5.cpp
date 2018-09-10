b196853dedd91862b31ef4a19a66a4951e176900
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-09-10 10:44:26 -0400

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,2 @@
+//Write a main function that asks user to input 2 variables and asks the user what operation does he want to perform. 
+//Based on his input, call the required function and display the output. 
