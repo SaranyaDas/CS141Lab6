ef74cdb59b6d0c6405e84257bcc12a02a217b891
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-09-10 11:01:28 -0400

    Update 5.cpp
    s

--- 5.cpp
+++ 5.cpp
@@ -1,2 +1,56 @@
-//Write a main function that asks user to input 2 variables and asks the user what operation does he want to perform. 
-//Based on his input, call the required function and display the output. 
+//Library
+#include <iostream>
+using namespace std;
+//Different functions
+int ADD (int x, int y) {
+  //Add them and return sum
+  int sum = x + y;
+  return sum;
+}
+int MAX (int x, int y) {
+//Find and return maximum
+  int z;
+  if (x > y) {
+    z = x;
+  }
+  else {
+    z = y;
+  }
+  return z;
+}
+int MIN (int x, int y) {
+//Find and return minimum
+  int z;
+  if (x < y) {
+    z = x;
+  }
+  else {
+    z = y;
+  }
+  return z;
+}
+
+int main () {
+  //Ask user to input 2 variables 
+  char n, int a, b;
+  cout << "Enter two numbers: " << endl;
+  cin >> a, b;
+  //Ask user their desired operation
+  cout << "Choose desired operation - finding the sum (ADD), maximum (MAX) or minimum (MIN) of two numbers: " << endl;
+  //Call required function and display output 
+  cin >> n;
+  if (n == ADD) {
+    cout << "The sum is " << ADD (a, b) << "." << endl;
+  }
+  else {
+    if (n == MAX) {
+      cout << "The maximum is " << MAX (a, b) << "." << endl;
+    }
+    else {
+      if (n == MIN) {
+        cout << "The minimum is " << MIN (a, b) << "." << endl;
+      }
+    }
+  }
+  return 0;
+}
