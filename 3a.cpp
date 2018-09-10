bafa5428f470845e09bb3d35c33295d2b4d51bf4
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-09-10 10:08:00 -0400

    Update 3a.cpp

--- 3a.cpp
+++ 3a.cpp
@@ -1,2 +1,26 @@
 //Library
 #include <iostream>
+using namespace std;
+//Function takes two int parameters,
+int MAX (int x, int y) {
+//Find and return maximum
+  int z;
+  if (x > y) {
+    z = x;
+  }
+  else {
+    z = y;
+  }
+  return z;
+}
+int main () {
+  //ask user for two numbers
+  int a, b, c;
+  cout << "Enter two numbers: " << endl;
+  cin >> a >> b;
+  //call function with numbers as arguments, 
+  c = MAX (a, b);
+  //display maximum
+  cout << "The maximum is " << c << "." << endl;
+  return 0;
+}
