afb2f70bef9203ab3d3826d77823276a9e9d0616
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-09-10 10:34:15 -0400

    Update 2a.cpp

--- 2a.cpp
+++ 2a.cpp
@@ -1,17 +1,18 @@
-//Librarytakes two int parameters,
+//Library
 #include <iostream>
 using namespace std;
-//Function to add two integers
+//Function takes two int parameters
 int flag (int a,int b) {
+  //Add them and return sum
   int sum = a + b;
   return sum;
 }
 int main() {
-  //Ask user for values
+  //Ask user for two numbers
   int a,b;
   cout<<"Enter two integers:" << endl;
   cin >> a >> b;
-  //Display sum
-  cout<< flag (a,b) <<" is the sum." << endl;
+  //Call function with numbers as arguments and display sum
+  cout << flag (a,b) << " is the sum." << endl;
   return 0;
 }
