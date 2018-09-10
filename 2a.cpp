2d699deaa80dc306b78aee067f42449cf332b2b8
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-09-10 10:47:35 -0400

    Update 2a.cpp

--- 2a.cpp
+++ 2a.cpp
@@ -2,17 +2,17 @@
 #include <iostream>
 using namespace std;
 //Function takes two int parameters
-int flag (int a,int b) {
+int flag (int x,int y) {
   //Add them and return sum
-  int sum = a + b;
+  int sum = x + y;
   return sum;
 }
 int main() {
   //Ask user for two numbers
-  int a,b;
+  int a, b;
   cout<<"Enter two integers:" << endl;
   cin >> a >> b;
   //Call function with numbers as arguments and display sum
-  cout << flag (a,b) << " is the sum." << endl;
+  cout << flag (a, b) << " is the sum." << endl;
   return 0;
 }
