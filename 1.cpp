c7aea23513c974a41c74667e505e50d345b1b9f7
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-09-10 11:34:23 -0400

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,6 @@
+//Tax Rate Calculation using Functions 
+//function that obtains from the user and returns a value for unitCst, units, and taxRt to the calling module.
+//function that receives the unitCst, units, and taxRt and returns to the calling module the salesTx and totDue. 
+//function that receives the unitCst, units, taxRt, salesTx, and totDue and outputs them in a very readable format.
+//main function with variables called unitCost, unitsPurch, taxRate, salesTax, and totalDue. 
+//program should call the functions listed above. 
