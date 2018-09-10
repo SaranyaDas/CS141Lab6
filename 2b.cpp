8eda7a5f69c0e9ffe473ea0a03bf5583de7059ba
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-09-10 10:17:31 -0400

    Update 2b.cpp

--- 2b.cpp
+++ 2b.cpp
@@ -1,22 +1,14 @@
 //Library
 #include <iostream>
 using namespace std;
-
-int A (int x, int y) {
-int w = x + y;
-return w;
+void flag (int x, int y, int &z) {
+z = x + y;
 }
-
-void B (int x, int y, int &z) {
-z = A (x, y);
-}
-
 int main () {
 int a, b, c;
 cout << "Enter two numbers: " << endl;
 cin >> a >> b;
-B (a, b, c);
+flag (a, b, c);
 cout << "The sum is " << c << "." << endl;
 return 0;
 }
-
